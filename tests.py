32ad3d6f24ff118be42383735582f3c66c3a5c66
onlyphantom <samuel@outlook.sg>
2018-09-22 13:42:13 +0700

    More unittests

--- tests.py
+++ tests.py
@@ -3,32 +3,49 @@ from app import app, db
 from app.users import User
 
 class UserModelClass(unittest.TestCase):
-    def setUp(self):
+
+    @classmethod
+    def setUpClass(cls):
+        print('setUp Class')
         app.config['SQLALCHEMY_DATABASE_URI'] \
         = 'mysql+pymysql://Samuel:tirab33@localhost/assis_testing'
         app.config['SQLALCHEMY_ECHO'] = False
         db.create_all()
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDown Class')
         db.session.remove()
         db.drop_all()
 
-    def test_add_user(self):
-        u = User(email='samuel@algorit.ma',
-                 leadership=True)
-        u.set_password('samuel@algorit.ma')
-        db.session.add(u)
+    def setUp(self):
+        print('SetUp')
+        self.u = User(email='samuel@algorit.ma', leadership=True)
+        self.u.set_password('samuel@algorit.ma')
+        self.l = User(email='tiara@algorit.ma', leadership=True)
+        self.l.set_password('tiara@algorit.ma')
+        self.r = User(email='ardhito@algorit.ma')
+        self.r.set_password('ardhito@algorit.ma')
+        db.session.add(self.u)
+        db.session.add(self.l)
+        db.session.add(self.r)
         db.session.commit()
-        returned_u = User.query.filter_by(email='samuel@algorit.ma').first()
+
+    def tearDown(self):
+        print('tearDown')
+        db.session.query(User).delete()
+        db.session.commit()
+
+    def test_user_creation(self):
+        returned_u = User.query.filter_by(email='samuel@algorit.ma').one()
+        returned_u2 = User.query.filter_by(email='ardhito@algorit.ma').one()
         self.assertEqual(returned_u.email, 'samuel@algorit.ma')
+        self.assertFalse(returned_u2.leadership)
 
     def test_password_hashing(self):
-        u = User(email='samuel@algorit.ma',
-                 leadership=True)
-        u.set_password('samuel@algorit.ma')
-        self.assertFalse(u.check_password('samuel123'))
-        self.assertTrue(u.check_password('samuel@algorit.ma'))
+        self.assertFalse(self.u.check_password('Samuel@algorit.ma'))
+        self.assertTrue(self.u.check_password('samuel@algorit.ma'))
 
-# to test, in Python console: python tests.py
+# to test, in console: python tests.py
 if __name__ == '__main__':
     unittest.main(verbosity=2)
