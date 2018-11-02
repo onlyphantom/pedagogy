b72cea68ab66dc7df92fc6383484f0e629c54516
onlyphantom <samuel@outlook.sg>
2018-11-02 21:05:54 +0700

    add flask g and add altair viz

--- app/analytics.py
+++ app/analytics.py
@@ -1,20 +1,39 @@
+from flask import g
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import Employee, Workshop, Response
 import altair as alt
 import pandas as pd
 from app import app
 from config import conn
-#from altair import Chart, X, Y, Axis, Data, DataFormat
 
 df = pd.read_sql_query("SELECT * FROM workshop", conn, index_col='id')
 # convert datetime to '2018-09' month and year format
 df['mnth_yr'] = df['workshop_start'].dt.to_period('M').astype(str)
-# TODO: change to compare to current user instead of hardcoded to Samuel
-df['this_user'] = df['workshop_instructor'] == 1
-melted = pd.melt(df[df['workshop_instructor'] == 1], 
-              id_vars=['mnth_yr', 'workshop_category'], 
-              value_vars=['workshop_hours', 'class_size'])
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        employee = Employee.query.filter_by(email=current_user.email).first()
+        df['this_user'] = df['workshop_instructor'] == employee.id
+        g.melted = pd.melt(
+            df[df['this_user'] == True],
+            id_vars=['mnth_yr', 'workshop_category'], 
+            value_vars=['workshop_hours', 'class_size'])
+
+        df2 = df.copy()
+        df2['workshop_category'] = df2['workshop_category'].astype('category')
+        df2['workshop_category'] = pd.Categorical(df2['workshop_category']).codes
+        print(df.head())
+        print(df2.head())
+        g.dat = df2.set_index('workshop_start').resample('W').sum()
+        g.accum = pd.melt(g.dat.reset_index(), 
+            id_vars=['workshop_start','workshop_category'], 
+            value_vars=['workshop_hours', 'class_size'])
+        g.accum['workshop_category'] = g.accum['workshop_category'].apply(lambda x: 'Public' if (x == 0 | x == 2) else 'Corporate')
+        g.accum['cumsum'] = g.accum.groupby(['variable','workshop_category']).cumsum().fillna(0)
 
 @app.route('/data/class_size_vs')
-def class_size_vs():
+def class_size_vs():  
     chart = alt.Chart(df).mark_area(
         opacity=0.75,
         interpolate='step'
@@ -30,11 +49,38 @@ def class_size_vs():
 
 @app.route('/data/class_size_hours')
 def class_size_hours():
-    chart = alt.Chart(melted).mark_bar().encode(
+    chart = alt.Chart(g.melted).mark_bar().encode(
         column='variable',
         x=alt.X("sum(value)"),
         y=alt.Y('mnth_yr'),
         color=alt.Color('workshop_category')
+    ).properties(
+        width=250
+    )
+    return chart.to_json()
+
+@app.route('/data/accum_personal')
+def accum():
+    chart = alt.Chart(g.accum).mark_area().encode(
+        column='workshop_category',
+        x=alt.X("workshop_start"),
+        y=alt.Y("sum(cumsum):Q"),
+        color=alt.Color("variable")
+    ).properties(
+        width=250
+    )
+    return chart.to_json()
+
+@app.route('/data/punchcode')
+def punchcode():
+    df['workshop_category'] = df['workshop_category'].apply(lambda x:x if x == 'Corporate' else 'Public' )
+    df['contrib'] = df['workshop_hours'] * df['class_size']
+
+    chart = alt.Chart(df).mark_circle().encode(
+        x='mnth_yr:O',
+        y='workshop_instructor:O',
+        size='sum(contrib):Q',
+        column='workshop_category:O'
     ).properties(
         width=250
     )
