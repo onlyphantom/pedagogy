624126deb131124872ccb50d17f7fd29dcad47eb
onlyphantom <samuel@outlook.sg>
2018-11-02 15:52:15 +0700

    altair in accomplishment page POC

--- app/analytics.py
+++ app/analytics.py
@@ -1,7 +1,29 @@
 import altair as alt
 import pandas as pd
 from app import app
+from config import conn
 #from altair import Chart, X, Y, Axis, Data, DataFormat
+
+df = pd.read_sql_query("SELECT * FROM workshop", conn, index_col='id')
+# convert datetime to '2018-09' month and year format
+df['mnth_yr'] = df['workshop_start'].dt.to_period('M').astype(str)
+# TODO: change to compare to current user instead of hardcoded to Samuel
+df['this_user'] = df['workshop_instructor'] == 1
+
+@app.route('/data/class_size_vs')
+def class_size_vs():
+    chart = alt.Chart(df).mark_area(
+        opacity=0.75,
+        interpolate='step'
+    ).encode(
+        alt.X("class_size:Q", bin=alt.Bin(maxbins=12)),
+        alt.Y('count()', stack=None),
+        alt.Color(
+            'this_user',
+            scale=alt.Scale(range=['#000000', '#62092f'])
+        )
+    )
+    return chart.to_json()
 
 @app.route('/data/mediumos')
 def mediumos():
