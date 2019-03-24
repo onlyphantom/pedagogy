36243cba87c45ca4e25e237869e3684859d63156
onlyphantom <samuel@outlook.sg>
2019-03-24 12:26:58 +0700

    asynchronous email #20 & better login page

--- app/email.py
+++ app/email.py
@@ -1,12 +1,17 @@
 from flask import render_template
 from flask_mail import Message
+from threading import Thread
 from app import app, mail # mail = flask_mail.Mail(app)
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 def send_pw_reset_email(user):
     token = user.get_reset_password_token()
@@ -17,5 +22,4 @@ def send_pw_reset_email(user):
             user=user, token=token),
         html_body=render_template('email/reset_password.html',
             user=user, token=token))
-
 
