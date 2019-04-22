27ff6621f2470d6a102bdb0fc1832098730ed582
Samuel Chan <samuel@outlook.sg>
2019-04-22 22:34:26 +0800

    added team analytics table

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, g
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db, cache
-from app.analytics import factory_homepage, factory_accomplishment
+from app.analytics import factory_homepage, factory_accomplishment, factory_analytics
 from app.users import User
 from app.models import Employee, Workshop, Response
 from app.forms import LoginForm, RegistrationForm, SurveyForm, ResetPasswordRequestForm, ResetPasswordForm
@@ -20,7 +20,7 @@ def before_request():
 @app.route('/index')
 @cache.cached(timeout=100)
 def index():
-    stats=factory_homepage()
+    stats = factory_homepage()
     return render_template('index.html', stats=stats)
 
 @app.route('/accomplishment')
@@ -30,7 +30,7 @@ def accomplishment():
     if g.employee is None:
         flash('Not registered as a Product team member yet. Check back later!')
         return redirect(url_for('index'))
-    personstats=factory_accomplishment(u=g.employee)
+    personstats = factory_accomplishment(u=g.employee)
     return render_template('accomplishment.html', personstats=personstats)
 
 @app.route('/explorer')
@@ -41,7 +41,8 @@ def explorer():
 
 @app.route('/analytics')
 def analytics():
-    return render_template('analytics.html')
+    instructorstats = factory_analytics()
+    return render_template('analytics.html', instructorstats=instructorstats)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
